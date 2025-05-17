f41fa3009bec8dc282b0e31f2ac591855800dd87
Maitri <maitri@Maitris-MacBook-Air.local>
2025-05-17 16:53:51 +0530

    Added avg_price, final price dist, prob above K

--- src/hestonModel.cpp
+++ src/hestonModel.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <chrono>
+#include <iomanip>
 
 using namespace std;
 
@@ -88,7 +90,40 @@ void plottMultiple(const vector<double>& x, const vector<vector<double>>& y_seri
     pclose(gnuplotPipe);
 }
 
+struct PriceStats {
+    double avg;
+    std::vector<double> prices;
+};
+
+PriceStats calc_fmetrics(int num_paths, int N, vector<vector<double>> all_S){
+    double avg_final_price = 0;
+    vector <double> prices;
+    for (int p = 0; p < num_paths; ++p){
+        avg_final_price += all_S[p][N];
+        prices.push_back(all_S[p][N]);
+    }
+    avg_final_price /= num_paths;
+
+    PriceStats result;
+    result.avg = avg_final_price;
+    result.prices = prices;
+    return {avg_final_price, prices}; 
+}
+
+double price_Kplus_prob(const std::vector<double>& prices, double K){
+    
+    int count = 0;
+    for (int i =0; i<prices.size(); ++i){
+        if (prices[i] > K) count ++;
+    }
+
+    return static_cast<double> (count)/prices.size();
+}
+
 int main(){
+
+    auto start = std::chrono::high_resolution_clock::now();
+
     float T = 1.0; //total time
     int N = 1000; // time steps
     double dt = T/N;
@@ -98,15 +133,16 @@ int main(){
     float kappa = 3.0; //Mean reversion pullback force
     float theta = 0.15; //Long term volatility
     float sigma = 0.1; //Volatility
+    double K = 110.0; //Price above ?
 
-    vector<double> dw1 = rng(0.0, dt, N);
-    vector<double> dw2 = rng(0.0, dt, N);
+    // vector<double> dw1 = rng(0.0, dt, N);
+    // vector<double> dw2 = rng(0.0, dt, N);
 
-    vector<double> S(N+1);
-    vector<double> v(N+1);
+    // vector<double> S(N+1);
+    // vector<double> v(N+1);
 
-    S[0] = 100.0;
-    v[0] = v0;
+    // S[0] = 100.0;
+    // v[0] = v0;
 
     int num_paths = 10;
     vector<vector<double>> all_S(num_paths, vector<double>(N+1));
@@ -127,6 +163,19 @@ int main(){
 
     plottMultiple(t, all_S, "Monte Carlo Paths: Stock Price");
     plottMultiple(t, all_v, "Monte Carlo Paths: Volatility");
+
+    PriceStats stats = calc_fmetrics(num_paths, N, all_S);
+    std::cout << "Average final price: " << stats.avg << std::endl;
+
+    cout<<fixed<<setprecision(4);
+    double prob = price_Kplus_prob(stats.prices, K);
+    std::cout << "Probability of price above " << K << " is " << prob <<endl;
+
+
+    //Timer ends
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double, std::milli> duration = end - start;
+    std::cout << "Execution time: " << duration.count() << " ms" << std::endl;
 
     return 0;
 }
