38399467fe483ee9b26de289fa22d789a5a6ab3a
Maitri <maitri@Maitris-MacBook-Air.local>
2025-05-18 23:09:34 +0530

    NA

--- include/constants.h
+++ include/constants.h
@@ -7,17 +7,17 @@
 extern const std::vector<double> t;
 extern const std::complex<double> z1;
 constexpr double S0 = 100.0;
-constexpr double r = 0.07; //risk_free_rate
-constexpr int num_paths = 1000;
+constexpr double r = 0.05; //risk_free_rate
+constexpr int num_paths = 1;
 constexpr double T = 1.0; //total time
 constexpr int N = 1000; // time steps
 constexpr double dt = T/N;
 constexpr double mu = 0.1; //expected return
-constexpr double v0 = 0.1;//Initial volatility
-constexpr double kappa = 3.0; //Mean reversion pullback force
+constexpr double v0 = 0.05;//Initial volatility
+constexpr double kappa = 2.0; //Mean reversion pullback force
 constexpr double theta = 0.15; //Long term volatility
-constexpr double sigma = 0.1; //Volatility of volatility
-constexpr double K = 110.0; //Strike price
+constexpr double sigma = 0.3; //Volatility of volatility
+constexpr double K = 100.0; //Strike price
 constexpr double rho = -0.5; //Coorelation coefficient
 
 #endif
